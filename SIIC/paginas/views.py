3c322f7243a87573cc2a734fa92bbe47a9bcead3
Aletrip-dev <aletrip@msn.com>
2021-03-01 17:20:54 -0300

    Criação de página de modelo e página sobre

--- SIIC/paginas/views.py
+++ SIIC/paginas/views.py
@@ -4,4 +4,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "paginas/modelo.html"
+
+
+class SobreView(TemplateView):
+    template_name = "paginas/sobre.html"
+
     
