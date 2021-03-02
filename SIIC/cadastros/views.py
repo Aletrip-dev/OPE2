26bf90ba5be83f67b0e849c1834dd72d5ba53d7b
Aletrip-dev <aletrip@msn.com>
2021-03-02 13:21:38 -0300

    testes no branch 3 do mac

--- SIIC/cadastros/views.py
+++ SIIC/cadastros/views.py
@@ -4,3 +4,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 # class IndexView(TemplateView):
 #     template_name = "paginas/modelo.html"
+
+
+#testes de produção no mac
