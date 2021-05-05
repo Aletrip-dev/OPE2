aa3bac71319481a853135f6532ff892f526ea5d5
admin <admin@admins-MacBook-Pro.local>
2021-05-04 21:34:01 -0300

    metodos para saída de produtos

--- SIIC/estoque/admin.py
+++ SIIC/estoque/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin.options import TabularInline
-from .models import EstoqueItens, Estoque
+from .models import EstoqueItens, Estoque, EstoqueEntrada, EstoqueSaida
 
 # Register your models here.
 
@@ -12,8 +12,17 @@ class EstoqueItensInline(TabularInline):
     extra = 0
 
 
-@admin.register(Estoque)
-class EstoqueAdmin(admin.ModelAdmin):
+@admin.register(EstoqueEntrada)
+class EstoqueEntradaAdmin(admin.ModelAdmin):
+    inlines = (EstoqueItensInline,)
+    list_display = ('__str__', 'funcionario', 'nf',
+                    'movimento', 'funcionario',)
+    search_fields = ('nf',)
+    date_hierarchy = 'created'
+
+
+@admin.register(EstoqueSaida)
+class EstoqueSaidaAdmin(admin.ModelAdmin):
     inlines = (EstoqueItensInline,)
     list_display = ('__str__', 'funcionario', 'nf',
                     'movimento', 'funcionario',)
